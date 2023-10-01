3d9d63a47b0ef6b22cb712824ab0ccdaf2d3bc25
Michiel van den Broek <michiel2003broek@gmail.com>
2023-10-01 14:33:10 +0200

    code cleanup and capsule cast

--- IGS_DOOM/Assets/Scripts/Player/Player.cs
+++ IGS_DOOM/Assets/Scripts/Player/Player.cs
@@ -58,8 +58,8 @@ namespace Player
 
         
         // can initialize from constructor
-        private float playerHeight = 2f;
-
+        private float playerHeight;
+        private float playerHalfHeight;
 
         private float jumpCoolDown;
         private bool readyToJump;
@@ -70,13 +70,14 @@ namespace Player
 
         private float airMultiplier = .4f;
         private float jumpForce = 8f;
-        private float groundDrag = 5f;
-        private float airDrag = 2f;
+        private float groundDrag = 1.5f;
+        private float airDrag = .5f;
         private float crouchYScale = .5f;
         private float walkSpeed = 5;
         private float runSpeed = 7;
         private float crouchSpeed = 6;
         private float maxSlopeAngle = 60;
+        private float vaultSpeed = .165f;
         
         public Player()
         {
@@ -115,6 +116,7 @@ namespace Player
             rb.interpolation = RigidbodyInterpolation.Interpolate;
             rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
 
+            // Attach functions to input
             input.Player.MouseXY.performed += SetMouse;
             input.Player.MouseXY.canceled += SetMouse;
             input.Player.Movement.started += MoveInput;
@@ -125,8 +127,15 @@ namespace Player
             input.Player.Jump.started += Jump;
             input.Player.Jump.performed += Jump;
 
-            startYScale = playerObject.transform.localScale.y;
+            InitPlayerVariables();
+        }
+
+        private void InitPlayerVariables()
+        {
             playerRadius = pCollider.radius;
+            playerHeight = pCollider.height;
+            playerHalfHeight = playerHeight / 2;
+            startYScale = playerObject.transform.localScale.y;
         }
 
         private void OnEnable()
@@ -149,16 +158,20 @@ namespace Player
             // Grounded check
             // done with CheckCapsule because raycast was very buggy on a little uneven terrain
             Vector3 groundStart = pCollider.bounds.center;
-            Vector3 groundEnd = new(pCollider.bounds.center.x, pCollider.bounds.min.y - 0.1f, pCollider.bounds.center.z);
+            Vector3 groundEnd = new (pCollider.bounds.center.x, pCollider.bounds.min.y - 0.1f, pCollider.bounds.center.z);
             isGrounded = Physics.CheckCapsule(groundStart, groundEnd, playerRadius, groundLayer);
             Debug.Log(isGrounded);
             
+            
             cam.UpdateCamera(mouseInput);
             
-            stateController.Update();
-
+            // clamp the speed before applying it to the player
             SpeedControl();
-
+            
+            // Update the state controller
+            stateController.Update();
+            
+            
             // MOVE THIS TO A STATE PLS
             LedgeGrab();
             
@@ -167,57 +180,54 @@ namespace Player
         }
         private void LedgeGrab()
         {
-            RaycastHit wallHit;
-            
-            Debug.DrawRay(pTransform.position, orientation.forward);
             // Check for wall
-            if (Physics.Raycast(pTransform.position + Vector3.up, orientation.forward, out wallHit, 1f, groundLayer))
+            RaycastHit wallHit;
+            var bounds = pCollider.bounds;
+            Vector3 point1 = bounds.center;
+            // point 2 should be lower but not to the ground (we do a separate check for the ground (or if we can split up the complete capsule we can use that),
+            // that will be a check to step up certain height ledges)
+            // ^^ probs in separate function StepUp()
+            Vector3 point2 = new (bounds.center.x, bounds.center.y + playerHalfHeight, bounds.center.z);
+            
+            // If there is a wall, raycast for ledgedetection
+            if (Physics.CapsuleCast(point1, point2, .1f, orientation.forward, out wallHit, 1f, groundLayer))  //Physics.Raycast(pTransform.position + Vector3.up, orientation.forward, out wallHit, 1f, groundLayer))
             {
                 //Debug.Log(wallHit.collider);
                 Debug.DrawRay(wallHit.point, Vector3.up, Color.blue);
                 RaycastHit capsuleHit;
-                float radius = .5f;
-                
                 // NEED TO FIGURE OUT, CAPSULE CAST TO SEE IF THERE IS ENOUGH SPACE FOR THE ENTIRE PLAYER
-                
                 //Vector3 capPos1 = wallHit.point + (orientation.forward * radius) + (Vector3.up * (.5f * playerHeight));
                 //Vector3 capPos2 = wallHit.point + (orientation.forward * radius) + (Vector3.down * (.5f * playerHeight));
                 //Debug.DrawLine(capPos1, capPos2, Color.green);
-                Vector3 downRay = wallHit.point + (orientation.forward * radius) + (Vector3.up * (.5f * playerHeight));
+                Vector3 downRay = wallHit.point + (orientation.forward * playerRadius) + (Vector3.up * (.5f * playerHeight));
                 
+                // check if there is a ledge, and enough room to stand onto the ledge
                 Debug.DrawRay(downRay, Vector3.down, Color.green);
-                if (Physics.Raycast(downRay,Vector3.down, out capsuleHit, playerHeight, groundLayer))  //Physics.Raycast(capPos1, capPos2, radius, orientation.forward, out capsuleHit))
+                if (Physics.Raycast(downRay,Vector3.down, out capsuleHit, playerHeight, groundLayer))  //Physics.CapsuleCast(capPos1, capPos2, radius, orientation.forward, out capsuleHit))
                 {
-                    // MAKE VARIABLE IN THE FUTURE FOR VAULTSPEED
-                    GameManager.Instance.StartCoroutine(LerpToVaultPos(capsuleHit.point, .175f));
-                    
-                 
-                    //Debug.Log(capsuleHit.collider);
+                    // lerp the player to the valid position
+                    GameManager.Instance.StartCoroutine(LerpToVaultPos(capsuleHit.point, vaultSpeed));
                 }
             }
             else
             {
                 Debug.Log("No Wall");
             }
-            
-            
-            // If there is a wall, raycast for ledgedetection
-            // if there is a ledge, and enough room to stand onto the ledge
-            // lerp te player to the ledgegrabbed position
-
         }
 
         private IEnumerator LerpToVaultPos(Vector3 _targetPos, float duration)
         {
             float time = 0;
             Vector3 startPos = pTransform.position;
-            Vector3 targetPos = new (_targetPos.x, _targetPos.y + playerHeight/2, _targetPos.z);
+            Vector3 targetPos = new (_targetPos.x, _targetPos.y + playerHalfHeight, _targetPos.z);
             while (time < duration)
             {
+                // move the player to the targetpos
                 rb.MovePosition(Vector3.Lerp(startPos, targetPos, time / duration));
                 time += Time.deltaTime;
                 yield return null;
             }
+            // Make sure the player is at the target pos at the end (lerp isn't exact)
             rb.MovePosition(targetPos);
         }
 
@@ -228,7 +238,7 @@ namespace Player
 
         public void PlayerMove()
         {
-            moveDirection = playerObject.transform.forward * moveInput.y + playerObject.transform.right * moveInput.x;
+            moveDirection = orientation.forward * moveInput.y + orientation.right * moveInput.x;
 
             if (OnSlope() && !exitingSlope)
             {
@@ -236,16 +246,18 @@ namespace Player
 
                 if (rb.velocity.y > 0)
                 {
-                    rb.AddForce(Vector3.down * 80, ForceMode.Force);
+                    rb.AddForce(Vector3.down * 100, ForceMode.Force);
                 }
             }
             
-            if (isGrounded)
+            if (isGrounded && !OnSlope())
                 rb.AddForce(moveDirection.normalized * (currentMoveSpeed * 10f), ForceMode.Force);
             else if (!isGrounded)
                 rb.AddForce(moveDirection.normalized * (currentMoveSpeed * 10f * airMultiplier), ForceMode.Force);
 
             rb.useGravity = !OnSlope();
+            Debug.Log(OnSlope().ToString());
+            Debug.Log(rb.velocity.magnitude);
         }
 
         private void SpeedControl()
@@ -259,14 +271,18 @@ namespace Player
             }
             else
             {
-                Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+                Vector3 flatVel = new (rb.velocity.x, 0f, rb.velocity.z);
 
                 if (flatVel.magnitude > currentMoveSpeed)
                 {
                     Vector3 limitedVel = flatVel.normalized * currentMoveSpeed;
-                    rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+                    rb.velocity = new (limitedVel.x, rb.velocity.y, limitedVel.z);
                 }
             }
+        }
+        private Vector3 GetSlopeMovementDirection()
+        {
+            return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
         }
 
         private void SetMouse(InputAction.CallbackContext value)
@@ -294,19 +310,19 @@ namespace Player
         {
             if (value.ReadValueAsButton())
             {
-                pTransform.localScale = new Vector3(pTransform.localScale.x, crouchYScale, pTransform.localScale.z);
+                pTransform.localScale = new (pTransform.localScale.x, crouchYScale, pTransform.localScale.z);
             
                 rb.AddForce(Vector3.down, ForceMode.Impulse);
             }
             else
             {
-                pTransform.localScale = new Vector3(pTransform.localScale.x, startYScale, pTransform.localScale.z);
+                pTransform.localScale = new (pTransform.localScale.x, startYScale, pTransform.localScale.z);
             }
         }
 
         private bool OnSlope()
         {
-            if (Physics.Raycast(playerObject.transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + .3f))
+            if (Physics.Raycast(pTransform.position, Vector3.down, out slopeHit, playerHalfHeight + .3f))
             {
                 float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
                 return angle < maxSlopeAngle && angle != 0;
@@ -315,16 +331,12 @@ namespace Player
             return false;
         }
 
-        private Vector3 GetSlopeMovementDirection()
-        {
-            return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
-        }
 
         private void PerformJump()
         {
             exitingSlope = true;
             
-            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            rb.velocity = new (rb.velocity.x, 0f, rb.velocity.z);
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
